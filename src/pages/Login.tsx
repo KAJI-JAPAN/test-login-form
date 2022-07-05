8f8934bcdd3a621c6afc7dcf3a668b972c040e1d
KAJI-JAPAN <78065333+KAJI-JAPAN@users.noreply.github.com>
2022-07-05 09:33:59 +0900

    模擬ログイン画面追加

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,16 @@
 import { Box, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
-import { memo, VFC } from "react";
+import { ChangeEvent, memo, useState, VFC } from "react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { UseAuth } from "../hooks/useOuth";
 
 export const Login: VFC = memo(() => {
+  const [userId, setUserId] = useState("");
+  const { login, loading } = UseAuth();
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+  const onClickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -11,8 +19,18 @@ export const Login: VFC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザーID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input
+            placeholder="ユーザーID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            onClick={onClickLogin}
+            loading={loading}
+            disabled={userId === ""}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
